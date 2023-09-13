4c2755a7b2720e960532eadb77dc411c839aa5ac
OtlichnikSasha <varlamov.sanya1990@mail.ru>
2023-09-14 00:03:13 +0300

    update/gh-pages-settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/NotesReactZustand/',
   plugins: [svgr({ include: '**/*.svg' }), react(), tsconfigPaths()],
   css: {
     preprocessorOptions: {
